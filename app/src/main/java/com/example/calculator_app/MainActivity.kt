c6c47feca6e52ac4775d756ee4c2bda5d522c5b4
soyeon <sykim0659@naver.com>
2024-09-05 15:21:02 +0900

    commit

--- app/src/main/java/com/example/calculator_app/MainActivity.kt
+++ app/src/main/java/com/example/calculator_app/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.calculator_app
 
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -19,10 +20,12 @@ class MainActivity : AppCompatActivity() {
     lateinit var eight: TextView
     lateinit var nine: TextView
     lateinit var zero: TextView
+
     lateinit var ca: TextView
     lateinit var equal: TextView
-    lateinit var result: TextView
     lateinit var plus: TextView
+
+    lateinit var result: TextView
 
     var input : String = "" //0
     var temp : String = "" //579
@@ -34,19 +37,39 @@ class MainActivity : AppCompatActivity() {
 
         findViews()
 
-        one.setOnClickListener {
-            input += (it as TextView).text
-            result.text = input
-        }
-        two.setOnClickListener {
-            input += (it as TextView).text
-            result.text = input
-        }
-        three.setOnClickListener {
-            input += (it as TextView).text
-            result.text = input
-        }
+        setNumberTextViewListener()
 
+
+        ca.setOnClickListener {
+            input = ""
+            temp = ""
+            result.text = "0"
+        }
+        plus.setOnClickListener {
+            temp = result.text.toString()
+            result.text = ""
+            input = ""
+        }
+        equal.setOnClickListener {
+            val finalResult :String = (input.toInt() + temp.toInt()).toString()
+            result.text = finalResult
+            temp = finalResult
+        }
+    }
+
+    fun setNumberTextViewListener(){
+        val numberTextViewList: List<TextView> = listOf(
+            one, two, three, four, five, six, seven, eight, nine, zero
+        )
+        val listener = object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                input += (p0 as TextView).text
+                result.text = input
+            }
+        }
+        numberTextViewList.forEach{
+            it.setOnClickListener (listener)
+        }
     }
 
     fun findViews(){
